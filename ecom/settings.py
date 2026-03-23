3044f827530b3970a0f6e6136252a6d6638b8ac7
alobit21 <macmtavangu@gmail.com>
2026-03-23 21:18:42 +0300

    Fix template DIRS path

--- ecom/settings.py
+++ ecom/settings.py
@@ -67,17 +67,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ecom.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # ✅ FIX
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.messages',
                 'cart.context_processors.cart',
             ],
         },
